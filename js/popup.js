5a2f3a6f84ea2fd58eff793634a13931eb3e94dc
aykutcevik <email@aykutcevik.com>
2020-04-05 14:35:20 +0200

    Added error handling in popup and added a retry function via snackbar

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,27 @@
 var geoIpV4 = null;
 var geoIpV6 = null;
+var ipv4IsFetching = true;
+var ipv6IsFetching = true;
+
+function reloadPopup() {
+    window.location.reload();
+}
+
+function handleError() {
+    if (geoIpV4 == null && geoIpV6 == null && !ipv4IsFetching && !ipv6IsFetching) {
+        setTimeout(function () { // https://github.com/google/material-design-lite/issues/1995
+            // connection issue            
+            var snackbarContainer = document.querySelector('.mdl-js-snackbar');
+            var data = {
+                message: 'Network error occured.',
+                timeout: 20000,
+                actionHandler: reloadPopup,
+                actionText: 'Retry'
+            };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        }, 1);
+    }
+}
 
 function fetchGeoLocation() {
     var geoLocate = new GeoLocation();
@@ -9,7 +31,9 @@ function fetchGeoLocation() {
             triggerView();
         },
         error: function () {
-            geoIpV4 = geoLocate;
+            geoIpV4 = null;
+            ipv4IsFetching = false;
+            handleError();
         }
     });
 
@@ -20,7 +44,9 @@ function fetchGeoLocation() {
             triggerView();
         },
         error: function () {
-            geoIpV6 = geoLocate6;
+            geoIpV6 = null;
+            ipv6IsFetching = false;
+            handleError();
         }
     });
 }
